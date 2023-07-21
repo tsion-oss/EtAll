9d9bf68ab8235748cf021261179cb86b8a5ca35f
Tsion <tsionwendmu12@gmail.com>
2023-07-21 16:14:20 -0400

    s

--- server.js
+++ server.js
@@ -4,28 +4,6 @@ const cors = require('cors')
 const db = require('./back-end/db/index')
 const PORT = process.env.PORT || 3001
 const AppRouter = require('./back-end/Router/AppRouter.js')
-// var logger = require('morgan');
-// new code below
-// var session = require('express-session');
-// var passport = require('passport');
-
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(session({
-//   secret: process.env.SECRET,
-//   resave: false,
-//   saveUninitialized: true
-// }));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// server.js
-
-// require('./config/database');
-// // new code below
-// require('./config/passport');
 
 app.use(cors())
 app.use(express.json())
